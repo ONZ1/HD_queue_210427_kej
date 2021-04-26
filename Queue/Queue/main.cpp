83f913713bc250734c0167e9cfe2b9327935a73f
ONZ1 <onzi1230@gmail.com>
2021-04-27 00:58:00 +0900

    Update main.cpp

--- Queue/Queue/main.cpp
+++ Queue/Queue/main.cpp
@@ -7,12 +7,13 @@ void main() {
 	AddedQueue(4);
 	AddedQueue(5);
 	AddedQueue(6);
-	/*AddedQueue(7);
-	AddedQueue(8);
+	AddedQueue(7);
+	/*AddedQueue(8);
 	AddedQueue(9); 
 	AddedQueue(10);
 	AddedQueue(11);
 	AddedQueue(12);*/
+
 	while (true)
 	{
 		if (IsQueueEmpty() == true)
