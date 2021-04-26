c0ee7a2b0433637c0e91d22286e54069ca221cfb
ONZ1 <onzi1230@gmail.com>
2021-04-27 00:47:48 +0900

    Create main.cpp

---
+++ Queue/Queue/main.cpp
@@ -0,0 +1,25 @@
+#include "queue.h"
+
+void main() {
+	AddedQueue(1);
+	AddedQueue(2);
+	AddedQueue(3);
+	AddedQueue(4);
+	AddedQueue(5);
+	AddedQueue(6);
+	/*AddedQueue(7);
+	AddedQueue(8);
+	AddedQueue(9); 
+	AddedQueue(10);
+	AddedQueue(11);
+	AddedQueue(12);*/
+	while (true)
+	{
+		if (IsQueueEmpty() == true)
+		{
+			printf("Á¾·á\n");
+			break;
+		}
+		else printf("%d\n", DeleteQueue());
+	}
+}
