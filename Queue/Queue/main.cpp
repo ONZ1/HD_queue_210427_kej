401b6f33e165b2f263776841c4c8fd335498c8dc
ONZ1 <onzi1230@gmail.com>
2021-04-27 18:14:22 +0900

    Update main.cpp
    코멘트 추가

--- Queue/Queue/main.cpp
+++ Queue/Queue/main.cpp
@@ -8,20 +8,20 @@ void main() {
 	AddedQueue(5);
 	AddedQueue(6);
 	AddedQueue(7);
-	/*AddedQueue(8);
+	AddedQueue(8);
 	AddedQueue(9); 
 	AddedQueue(10);
-	AddedQueue(11);
-	AddedQueue(12);*/
+	AddedQueue(11);		//초과값 예시
+	AddedQueue(12);
 
 	while (true)
 	{
-		if (IsQueueEmpty() == true)
+		if (IsQueueEmpty() == true)		//비어있으면 트루값이 리턴됨
 		{
 			printf("종료\n");
 			break;
 		}
-		else printf("%d\n", DeleteQueue());
+		else printf("%d\n", DeleteQueue());		//비어있지 않으면 큐 값을 모두 불러온 뒤 비워지면 종료
 	}
 
 	fgetc(stdin);
